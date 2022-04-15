57c1b52589ab4e5d612a14df68113fa11cc7245f
nmb6 <neha_borulkar@sfu.ca>
2022-04-14 23:11:27 -0700

    Added HTML files

---
+++ UI/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('#homePage').click(function () {
+    $(this).load('<embed type="text/html" src="indian_rest.html" width="800" height="600">')
+    // if (id="indian"){$(this).load('indian_rest.html');}
+    // else if (id="japanese"){$(this).load('japanese_rest.html');}
+    // else if (id="mexican"){$(this).load('mexican_rest.html');}
+    // else {$(this).load('nightlife_rest.html');}    
+   });
+});
